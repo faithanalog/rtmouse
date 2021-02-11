d082ccb60b8ddfccce7133564c56d3105e2ee730
artemis everfree <mail@artemis.sh>
2021-02-11 02:20:35 -0800

    LICENSE

--- rtmouse.c
+++ rtmouse.c
@@ -1,3 +1,31 @@
+/***************************************************************************
+                        rtmouse - based on kmousetool
+                             -------------------
+    == kmousetool authors ==
+    begin                : Sun Jan 20 23:27:58 PST 2002
+    copyright            : (C) 2002-2003 by Jeff Roush
+    email                : jeff@mousetool.com
+    copyright            : (C) 2003 by Olaf Schmidt
+    email                : ojschmidt@kde.org
+    copyright            : (C) 2003 by Gunnar Schmi Dt
+    email                : gunnar@schmi-dt.de
+    copyright            : (C) 2021 by Artemis Everfree
+    email                : mail@artemis.sh
+
+    == rtmouse authors ==
+    begin                : 2021
+    copyright            : (C) 2021 by Artemis Everfree
+    email                : mail@artemis.sh
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include <errno.h>
 #include <signal.h>
 #include <stdbool.h>
